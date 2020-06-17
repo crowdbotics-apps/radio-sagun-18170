2ee50685c2e06055321e4cc37fd61f810576fcee
Crowdbotics <team@crowdbotics.com>
2020-06-17 21:55:50 +0000

    Update models

--- backend/radio_sagun_18170/wsgi.py
+++ backend/radio_sagun_18170/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'radio_sagun_18170.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "radio_sagun_18170.settings")
 
 application = get_wsgi_application()
